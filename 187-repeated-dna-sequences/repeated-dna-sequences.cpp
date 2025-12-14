e26614372408ac5f9755d193e09f6aa22121f3e0
hk12maddheshiya <121261878+hk12maddheshiya@users.noreply.github.com>
2025-12-14 17:12:40 +0530

    Time: 87 ms (10.70%) | Memory: 24.8 MB (67.20%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n=s.size();
+        map<string,int> mpp;
+        vector<string> ans;
+        for(int i=0;i<= n-10;i++){
+            string temp=s.substr(i,10);
+            mpp[temp]++;
+         }
+         
+        for(auto it: mpp){
+            if(it.second >= 2) ans.push_back(it.first);
+        }
+        
+        
+        return ans;
+
+    }
+};
