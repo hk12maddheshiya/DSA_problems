82d7279cf5e087d92fef837523bebcb63575bcc9
hk12maddheshiya <121261878+hk12maddheshiya@users.noreply.github.com>
2025-12-15 17:09:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Swaps to Group 1s/min-swaps-to-group-1s.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+
+        int windowSize = 0;
+        for (int x : nums) if (x == 1) windowSize++;
+        if(windowSize == 0) return -1;
+       
+
+        int cz = 0;
+        
+        for (int i = 0; i < windowSize; i++) if (nums[i] == 0) cz++;
+        
+
+        int mini = cz;
+
+        for (int start = 0, end = windowSize; end < n; start++, end++) {
+            if (nums[end % n] == 0) cz++;
+            if (nums[start] == 0) cz--;
+            mini = min(mini, cz);
+        }
+
+        return mini;
+    }
+};
